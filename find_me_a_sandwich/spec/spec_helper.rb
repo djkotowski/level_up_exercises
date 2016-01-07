7fb40bc79e4a8142b808063914a1ef3530d34f27
creativisionist <creativisionist@gmail.com>
2016-01-07 17:15:14 -0600

    Silo fixtures.

--- find_me_a_sandwich/spec/spec_helper.rb
+++ find_me_a_sandwich/spec/spec_helper.rb
@@ -18,9 +18,3 @@ RSpec.configure do |config|
     DatabaseCleaner.cleaning { example.run }
   end
 end
-
-VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr"
-  config.hook_into(:webmock)
-  config.allow_http_connections_when_no_cassette = true
-end
